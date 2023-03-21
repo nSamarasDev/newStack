c591f584ce57323be7aa4d068ff2fb163ca91442
“[Nicholas <n.samaras4@outlook.com>
2023-03-21 12:54:58 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const colors = require("colors");
+const morgan = require("morgan");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect to db
+connectDB();
+
+// init middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 
