1938deffd76e1cba314e9592d654924732236fb8
“[Nicholas <n.samaras4@outlook.com>
2023-03-21 13:53:40 -0400

    update

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["user", "company"],
+    default: "user",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoosemodel("user", UserSchema);
